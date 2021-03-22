16f89bb0a50aaf99ba49e847206a91fd4f790348
Xendergo <h.rovnyak@gmail.com>
2021-03-22 16:13:24 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DSharpPlus;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace Botter_rewrite
+{
+    class Program
+    {
+        public static Dictionary<string, ICommand> commands = new Dictionary<string, ICommand>();
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("settings.json").Build();
+            MainAsync(config.GetConnectionString(args[0])).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string token) {
+            DiscordClient client = new DiscordClient(new DiscordConfiguration() {
+                Token = token,
+                TokenType = TokenType.Bot
+            });
+
+            await client.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
