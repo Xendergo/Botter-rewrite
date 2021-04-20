9439994e64443afb3b55bff847e407b19c5f7323
Xendergo <h.rovnyak@gmail.com>
2021-04-20 13:32:58 -0400

    Battle command interface

---
+++ Commands/IBattleCommand.cs
@@ -0,0 +1,34 @@
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.Entities;
+
+abstract class IBattleCommand : ICommand {
+  public abstract string help {get;}
+  public abstract string helpShort {get;}
+  // If the command involves doing something to another player, the other player must be called `target` in the signature
+  public abstract string[] signature {get;}
+  public abstract TypoableString[] aliases {get;}
+  public string category {get;} = "Rpg";
+  public bool admin {get;} = false;
+  public async Task Exec(DiscordClient client, Args args, DiscordMessage msg, Guild guild, User user) {
+    if (user.battle is null) {
+      await msg.RespondAsync("You must be in a battle to run this command, try using the `battlerequest` command to start a battle");
+      return;
+    }
+
+    if (args.users.ContainsKey("target")) {
+      User target = await Database.getUser(args.users["target"]);
+
+      if (target.battle != user.battle) {
+        await msg.RespondAsync($"You must be in the same battle to use this command, to invite them to your battle, run `botter battlerequest @{user.username}`");
+        return;
+      }
+    }
+
+    await Exec(client, args, msg, guild, user, user.battle);
+  }
+
+  protected abstract Task Exec(DiscordClient client, Args args, DiscordMessage msg, Guild guild, User user, Battle battle);
+}
